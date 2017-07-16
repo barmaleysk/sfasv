9653cf62ba06cacdab189d1db439b38521ce1d28
turboSlowPoke <mega_pokemon@mail.ru>
2017-07-17 00:34:46 +0300

    решил проблему с MySQL, в базе указал SET GLOBAL time_zone = '+4:00';

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,9 +1,13 @@
 package main;
 
 import database_service.DbService;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+import serlets.TestServlet;
 import telegram_services.WebhookService;
 
 /**
@@ -17,17 +21,26 @@ public class Main {
     private static final String pathToCertificateStore = "./keystore.jks"; //self-signed and non-self-signed.
     private static final String certificateStorePassword = "megapokemon"; //password for your certificate-store
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
         DbService dbService = new DbService();
         System.out.println("DbService запущен");
         ApiContextInitializer.init();
+        WebhookService webhookService = new WebhookService(dbService);
         try {
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(pathToCertificateStore,certificateStorePassword,EXTERNALWEBHOOKURL,INTERNALWEBHOOKURL,pathToCertificatePublicKey);
-            telegramBotsApi.registerBot(new WebhookService(dbService));
+            telegramBotsApi.registerBot(webhookService);
             System.out.println("TelegramService запущен");
         } catch (TelegramApiRequestException e) {
             System.out.println("Не смог создать telegramBotsApi для Webhook");
             e.printStackTrace();
         }
+        TestServlet testServlet = new TestServlet(webhookService);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(testServlet),"/*");
+        Server server = new Server(8080);
+        server.setHandler(context);
+        server.start();
+        System.out.println("Jetty started");
+        server.join();
     }
 }
